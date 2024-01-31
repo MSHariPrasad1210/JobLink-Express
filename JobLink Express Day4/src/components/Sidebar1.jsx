6118c17af1749d4e46a6ded4feed4e427a484dce
HARI PRASAD M S <125945504+MSHariPrasad1210@users.noreply.github.com>
2024-01-31 09:54:05 +0530

    Add files via upload

---
+++ JobLink Express Day4/src/components/Sidebar1.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import '../assets/css/Sidebar.css';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+  return (
+    
+    <div className="wrapper">
+      <div className="sidebar">
+        <div className="profile">
+          {/* <img
+            src="https://1.bp.blogspot.com/-vhmWFWO2r8U/YLjr2A57toI/AAAAAAAACO4/0GBonlEZPmAiQW4uvkCTm5LvlJVd_-l_wCNcBGAsYHQ/s16000/team-1-2.jpg"
+            alt="profile_picture"
+          /> */}
+          <h3>Hari Prasad M S</h3>
+          <p>Designer</p>
+        </div>
+        <ul>
+          <li>
+          <Link to="/dashboard">
+              <span className="icon">
+                <i className="fas fa-desktop"></i>
+              </span>
+              <span className="item">My Dashboard</span>
+              </Link>
+          </li>
+          <li>
+            <Link to={"/"}>
+              <span className="icon">
+                <i className="fas fa-user-friends"></i>
+              </span>
+              <span className="item">Add Resume</span>
+            </Link>
+          </li>
+          <li>
+          <Link to="/profile">
+              <span className="icon">
+                <i className="fas fa-tachometer-alt"></i>
+              </span>
+              <span className="item">My Profile</span>
+              </Link>
+          </li>
+          <li>
+            <Link to="/addprofile">
+              <span className="icon">
+                <i className="fas fa-database"></i>
+              </span>
+              <span className="item">Add Profile</span>
+              </Link>
+          </li>
+          <li>
+          <Link to="/appliedjobs">
+              <span className="icon">
+                <i className="fas fa-chart-line"></i>
+              </span>
+              <span className="item">Applied Jobs</span>
+              </Link>
+          </li>
+          <li>
+            <a href="#">
+              <span className="icon">
+                <i className="fas fa-user-shield"></i>
+              </span>
+              <span className="item">ApplyJob</span>
+            </a>
+          </li>
+          <li>
+          <Link to="/changepassword">
+              <span className="icon">
+                <i className="fas fa-cog"></i>
+              </span>
+              <span className="item">Change Password</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
